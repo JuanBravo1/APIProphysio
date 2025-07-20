4d7cb58d8daae4613e87f29381168f5b384816c4
Juan Bravo <20221029@uthh.edu.mx>
2025-07-20 16:27:41 -0600

    Agregado flask-cors a app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # <-- Importar esto
+from flask_cors import CORS
 import joblib
 import numpy as np
 
 app = Flask(__name__)
-CORS(app)
 
+# Permitir solo localhost:3000
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 modelo_tipo = joblib.load('modelo_tipo_terapia.pkl')
 modelo_sesiones = joblib.load('Sesiones_red_neuronal.pkl')
 modelo_semanas = joblib.load('Semanas_red_neuronal.pkl')
 modelo_costos = joblib.load('Costos_red_neuronal.pkl')
 
-@app.route('/predecir', methods=['POST'])
+@app.route('/predecir', methods=['POST', 'OPTIONS'])  # <-- Asegura que también acepta OPTIONS
 def predecir():
+    if request.method == 'OPTIONS':
+        return '', 200  # Responder correctamente a la preflight
     data = request.get_json()
 
     # Extraer variables necesarias
@@ -23,15 +26,15 @@ def predecir():
     tiempo = data.get('Tiempo_con_los_sintomas')
     dolor = data.get('Dolor')
     movilidad = data.get('Nivel_movilizacion_actual')
-
+    
     # Modelo 1: Tipo de terapia
     tipo_input = [[enfermedad, diagnostico, edad]]
     tipo_pred = modelo_tipo.predict(tipo_input)[0]
-
+   
     # Modelo 2: Número de sesiones
     sesiones_input = [[tiempo, dolor, movilidad]]
     sesiones_pred = modelo_sesiones.predict(sesiones_input)[0]
-
+    
     # Modelo 3: Semanas de recuperación
     semanas_input = [[sesiones_pred]]
     semanas_pred = modelo_semanas.predict(semanas_input)[0]
